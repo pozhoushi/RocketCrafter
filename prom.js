9191731d21e0a51e98b3021018e897c68360cbe3
liuyi <liuyi960422@163.com>
2021-03-31 16:08:06 +0800

    before leave

--- prom.js
+++ prom.js
@@ -1,97 +1,102 @@
-const isFunction = obj => typeof obj === 'function'
-const isObject = obj => !!(obj && typeof obj === 'object')
-const isThenable = obj => (isFunction(obj) || isObject(obj)) && 'then' in obj
-const isPromise = promise => promise instanceof Promise
-
-const PENDING = 'pending'
-const FULFILLED = 'fulfilled'
-const REJECTED = 'rejected'
-
-function Promise(f) {
-  this.result = null
-  this.state = PENDING
-  this.callbacks = []
-
-  let onFulfilled = value => transition(this, FULFILLED, value)
-  let onRejected = reason => transition(this, REJECTED, reason)
-
-  let ignore = false
-  let resolve = value => {
-    if (ignore) return
-    ignore = true
-    resolvePromise(this, value, onFulfilled, onRejected)
-  }
-  let reject = reason => {
-    if (ignore) return
-    ignore = true
-    onRejected(reason)
-  }
-
-  try {
-    f(resolve, reject)
-  } catch (error) {
-    reject(error)
-  }
+const isType = (target, str) => {
+  let rst = Object.prototype.toString.call(target)
+  return rst.substring(8, rst.length - 1).toLowerCase() === str
 }
 
-Promise.prototype.then = function(onFulfilled, onRejected) {
-  return new Promise((resolve, reject) => {
-    let callback = { onFulfilled, onRejected, resolve, reject }
+function Prom(f) {
+  this.state = 'pending'
+  this.result = null
+  this.callbacks = []
+  this.once = 0
+  let onFulfilled = newValue => tran(this, 'fulfilled', newValue)
+  let onRejected = newReason => tran(this, 'rejected', newReason)
+  let resolve = value => !this.once++ && resolvePromise(this, value, onFulfilled, onRejected)
+  let reject = reason => !this.once++ && onRejected(reason) 
+  try { f(resolve, reject) } catch (e) { reject(e) }
+}
 
-    if (this.state === PENDING) {
-      this.callbacks.push(callback)
-    } else {
-      setTimeout(() => handleCallback(callback, this.state, this.result), 0)
-    }
+const tran = (prom, newState, newResult) => {
+  if (newState !== 'pending') return
+  prom.state = newState
+  prom.result = newResult
+  setTimeout(_ => handleCallbacks(prom), 0)
+}
+
+const handleCallbacks = prom => {
+  while (prom.callbacks.length) handleCallback(prom.callbacks.shift(), prom.state, prom.result)
+}
+
+Prom.prototype.then = function (onFulfilled, onRejected) {
+  return new Promise((resolve, reject) => {
+    
   })
 }
 
-const handleCallback = (callback, state, result) => {
-  let { onFulfilled, onRejected, resolve, reject } = callback
-  try {
-    if (state === FULFILLED) {
-      isFunction(onFulfilled) ? resolve(onFulfilled(result)) : resolve(result)
-    } else if (state === REJECTED) {
-      isFunction(onRejected) ? resolve(onRejected(result)) : reject(result)
-    }
-  } catch (error) {
-    reject(error)
-  }
+const handleCallback = prom => {
+
 }
 
-const handleCallbacks = (callbacks, state, result) => {
-  while (callbacks.length) handleCallback(callbacks.shift(), state, result)
+// function Prom(f) {
+//   this.state = 'pending'
+//   this.result = null
+//   this.callbacks = []
+//   this.once = 0
+//   let onFulfilled = value => transition(this, 'fulfilled', value)
+//   let onRejected = reason => transition(this, 'rejected', reason)
+//   let resolve = value => !this.once++ && resolvePromise(this, value, onFulfilled, onRejected)
+//   let reject = reason => !this.once++ && onRejected(reason)
+//   try { f(resolve, reject) } catch (e) { reject(e) }
+// }
+
+// const transition = (promise, state, result) => {
+//   if (promise.state !== 'pending') return
+//   Object.assign(promise, {state, result})
+//   setTimeout(() => handleCallbacks(promise), 0)
+// }
+
+// Prom.prototype.then = function(onFulfilled, onRejected) {
+//   return new Prom((prom2resolve, prom2reject) => {
+//     let callback = { onFulfilled, onRejected, prom2resolve, prom2reject }
+//     this.state === 'pending' ?
+//       this.callbacks.push(callback) :
+//       setTimeout(() => handleCallback(callback, this.state, this.result), 0)
+//   })
+// }
+
+// const handleCallback = (callback, state, result) => {
+//   let { onFulfilled, onRejected, prom2resolve, prom2reject } = callback
+//   try {
+//     state === 'fulfilled' && ( isType(onFulfilled, 'function') ? prom2resolve(onFulfilled(result)) : prom2resolve(result) )
+//     state === 'rejected' && ( isType(onRejected, 'function') ? prom2resolve(onRejected(result)) : prom2reject(result) )
+//   } catch (error) { prom2reject(error) }
+// }
+
+// const handleCallbacks = prom => {
+//   while (prom.callbacks.length) handleCallback(prom.callbacks.shift(), prom.state, prom.result)
+// }
+
+// const resolvePromise = (promise, result, resolve, reject) => {
+//   if (result === promise) return reject(new TypeError('Can not fulfill promise with itself'))
+//   // if (result instanceof Promise) return result.then(resolve, reject)
+//   if (
+//     ( isType(result, 'function') || isType(result, 'object') ) &&
+//     'then' in result
+//   ) {
+//     try {
+//       let then = result.then
+//       if (isType(then, 'function')) return new Prom(then.bind(result)).then(resolve, reject)
+//     } catch (error) {
+//       return reject(error)
+//     }
+//   }
+//   resolve(result)
+// }
+
+/* npm run test */
+Prom.defer = Prom.deferred = function () {
+  let dfd = {}
+  dfd.promise = new Prom((resolve,reject) => Object.assign(dfd, { resolve, reject }))
+  return dfd;
 }
 
-const transition = (promise, state, result) => {
-  if (promise.state !== PENDING) return
-  promise.state = state
-  promise.result = result
-  setTimeout(() => handleCallbacks(promise.callbacks, state, result), 0)
-}
-
-const resolvePromise = (promise, result, resolve, reject) => {
-  if (result === promise) {
-    let reason = new TypeError('Can not fufill promise with itself')
-    return reject(reason)
-  }
-
-  if (isPromise(result)) {
-    return result.then(resolve, reject)
-  }
-
-  if (isThenable(result)) {
-    try {
-      let then = result.then
-      if (isFunction(then)) {
-        return new Promise(then.bind(result)).then(resolve, reject)
-      }
-    } catch (error) {
-      return reject(error)
-    }
-  }
-
-  resolve(result)
-}
-
-module.exports = Promise
+module.exports = Prom
